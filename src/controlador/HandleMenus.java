885eaf05a37ccd1e18af6330f13e6aa57d7b991a
arui <arucadev@gmail.com>
2025-05-04 20:26:42 +0200

    Update HandleMenus.java
    Combina totes les versions del menú amb totes les trucades a funcions de Escoles, Sectors, Escaladors i Vies

--- src/controlador/HandleMenus.java
+++ src/controlador/HandleMenus.java
@@ -3,6 +3,10 @@ package controlador;
 import controlador.objectes.escaladors.*;
 import controlador.objectes.escoles.*;
 import controlador.objectes.sectors.*;
+import controladors.objectes.vies.CrearVia;
+import controladors.objectes.vies.EliminarVia;
+import controladors.objectes.vies.LlistarVia;
+import controladors.objectes.vies.ModificarVia;
 import dao.SQLite.SQLiteEscaladorDAO;
 import dao.SQLite.SQLiteEscolaDAO;
 import dao.SQLite.SQLiteSectorDAO;
@@ -81,7 +85,7 @@ public class HandleMenus {
             option = parseInt(scan.nextLine());
             switch (option) {
                 case 1:
-                    // handleMenuTipusVia(vista, scan);
+                    handleMenuTipusVia(vista, scan);
                     break;
                 case 2:
                     System.out.println("Crear Escola...");
@@ -104,6 +108,54 @@ public class HandleMenus {
         } while (option != 0);
     }
 
+    private static void handleMenuTipusVia(Vista vista, Scanner scan) {
+        int option;
+        do {
+            vista.menuTipusVia();
+            System.out.print("Selecciona una opció: ");
+            option = parseInt(scan.nextLine());
+            switch (option) {
+                case 1:
+                    System.out.println("Crear Via Esportiva...");
+                    CrearVia.crearViaEsportiva();
+                    break;
+                case 2:
+                    System.out.println("Crear Via Clàssica...");
+                    CrearVia.crearViaClassica();
+                    handleCrearTrams(scan);
+                    break;
+                case 3:
+                    System.out.println("Crear Via Gel...");
+                    CrearVia.crearViaGel();
+                    handleCrearTrams(scan);
+                    break;
+                case 0:
+                    System.out.println("Tornar enrere...");
+                    break;
+                default:
+                    System.out.println("Opció incorrecte. Introdueix una opció vàlida.");
+            }
+        } while (option != 0);
+    }
+
+    private static void handleCrearTrams(Scanner scan) {
+        System.out.println("Introdueix els Trams per a la Via:");
+        boolean addMoreTrams = true;
+        while (addMoreTrams) {
+            System.out.print("Introdueix el llarg del tram: ");
+            double llarg = Double.parseDouble(scan.nextLine());
+            System.out.print("Introdueix el grau de dificultat del tram: ");
+            String grauDificultat = scan.nextLine();
+
+            System.out.println("Tram creat: Llarg = " + llarg + ", Grau de Dificultat = " + grauDificultat);
+
+            System.out.print("Vols afegir un altre tram? (s/n): ");
+            String response = scan.nextLine().toLowerCase();
+            addMoreTrams = response.equals("s");
+        }
+        System.out.println("Tots els trams han estat afegits.");
+    }
+
     private static void handleMenuModificar(Vista vista, Scanner scan, SQLiteEscolaDAO escolaDAO, SQLiteSectorDAO sectorDAO, SQLiteEscaladorDAO escaladorDAO, SQLiteViaDAO viaDAO, Connection connection) {
         int option;
         do {
@@ -113,6 +165,7 @@ public class HandleMenus {
             switch (option) {
                 case 1:
                     System.out.println("Modificar Via...");
+                    handleModificarVia(vista, scan);
                     break;
                 case 2:
                     System.out.println("Modificar Escola...");
@@ -135,6 +188,34 @@ public class HandleMenus {
         } while (option != 0);
     }
 
+    private static void handleModificarVia(Vista vista, Scanner scan) {
+        int option;
+        do {
+            vista.menuTipusVia(); // Reutilizando el menú de tipos de vía
+            System.out.print("Selecciona el tipus de via a modificar: ");
+            option = parseInt(scan.nextLine());
+            switch (option) {
+                case 1:
+                    System.out.println("Modificar Via Esportiva...");
+                    ModificarVia.modificarViaEsportiva();
+                    break;
+                case 2:
+                    System.out.println("Modificar Via Clàssica...");
+                    ModificarVia.modificarViaClassica();
+                    break;
+                case 3:
+                    System.out.println("Modificar Via Gel...");
+                    ModificarVia.modificarViaGel();
+                    break;
+                case 0:
+                    System.out.println("Tornar enrere...");
+                    break;
+                default:
+                    System.out.println("Opció incorrecte. Introdueix una opció vàlida.");
+            }
+        } while (option != 0);
+    }
+
     private static void handleMenuLlistar(Vista vista, Scanner scan, SQLiteEscolaDAO escolaDAO, SQLiteSectorDAO sectorDAO, SQLiteEscaladorDAO escaladorDAO, SQLiteViaDAO viaDAO, Connection connection) {
         int option;
         do {
@@ -143,7 +224,7 @@ public class HandleMenus {
             option = parseInt(scan.nextLine());
             switch (option) {
                 case 1:
-                    // handleSubMenuLlistarVies(vista, scan);
+                    handleSubMenuLlistarVies(vista, scan);
                     break;
                 case 2:
                     System.out.println("Llistar Escola...");
@@ -166,6 +247,46 @@ public class HandleMenus {
         } while (option != 0);
     }
 
+    private static void handleSubMenuLlistarVies(Vista vista, Scanner scan) {
+        int option;
+        do {
+            vista.subMenuLlistarVies();
+            System.out.print("Selecciona una opció: ");
+            option = parseInt(scan.nextLine());
+            switch (option) {
+                case 1:
+                    System.out.println("Llistar totes les vies...");
+                    LlistarVia.llistarTotesLesVies();
+                    break;
+                case 2:
+                    System.out.println("Llistar vies d'una escola...");
+                    LlistarVia.llistarViesEscola();
+                    break;
+                case 3:
+                    System.out.println("Llistar vies per dificultat...");
+                    LlistarVia.llistarViesPerDificultat();
+                    break;
+                case 4:
+                    System.out.println("Llistar vies segons estat...");
+                    LlistarVia.llistarViesPerEstat();
+                    break;
+                case 5:
+                    System.out.println("Llistar vies que han passat a 'Apte' recentment...");
+                    LlistarVia.llistarViesAptesRecentment();
+                    break;
+                case 6:
+                    System.out.println("Llistar vies més llargues d'una escola...");
+                    LlistarVia.llistarViesMesLlargues();
+                    break;
+                case 0:
+                    System.out.println("Tornar enrere...");
+                    break;
+                default:
+                    System.out.println("Opció incorrecte. Introdueix una opció vàlida.");
+            }
+        } while (option != 0);
+    }
+
     private static void handleMenuEliminar(Vista vista, Scanner scan, SQLiteEscolaDAO escolaDAO, SQLiteSectorDAO sectorDAO, SQLiteEscaladorDAO escaladorDAO, SQLiteViaDAO viaDAO, Connection connection) {
         int option;
         do {
@@ -175,6 +296,7 @@ public class HandleMenus {
             switch (option) {
                 case 1:
                     System.out.println("Eliminar Via...");
+                    handleEliminarVia(vista, scan);
                     break;
                 case 2:
                     System.out.println("Eliminar Escola...");
@@ -187,6 +309,34 @@ public class HandleMenus {
                 case 4:
                     System.out.println("Eliminar Escalador...");
                     new EliminarEscalador(escaladorDAO, scan, connection).eliminar();
+                    break;
+                case 0:
+                    System.out.println("Tornar enrere...");
+                    break;
+                default:
+                    System.out.println("Opció incorrecte. Introdueix una opció vàlida.");
+            }
+        } while (option != 0);
+    }
+
+    private static void handleEliminarVia(Vista vista, Scanner scan) {
+        int option;
+        do {
+            vista.menuTipusVia(); // Reutilizando el menú de tipos de vía
+            System.out.print("Selecciona el tipus de via a eliminar: ");
+            option = parseInt(scan.nextLine());
+            switch (option) {
+                case 1:
+                    System.out.println("Eliminar Via Esportiva...");
+                    EliminarVia.eliminarViaEsportiva();
+                    break;
+                case 2:
+                    System.out.println("Eliminar Via Clàssica...");
+                    EliminarVia.eliminarViaClassica();
+                    break;
+                case 3:
+                    System.out.println("Eliminar Via Gel...");
+                    EliminarVia.eliminarViaGel();
                     break;
                 case 0:
                     System.out.println("Tornar enrere...");
@@ -269,7 +419,7 @@ public class HandleMenus {
                     System.out.println("Mostrar les vies que han passat a \"Apte\" recentment");
                     break;
                 case 8:
-                    System.out.println("Mostrar les vies més llargues d’una escola determinada");
+                    System.out.println("Mostrar les vies més llargues d'una escola determinada");
                     break;
                 case 0:
                     System.out.println("Tornar enrere...");
